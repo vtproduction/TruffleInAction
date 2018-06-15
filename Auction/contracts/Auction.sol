1b78b22410b6c439ac1ab095f7d8a17b70ccbb33
Lê Mạnh Niên <nienbkict@gmail.com>
2018-06-15 17:51:30 +0700

    Auction: add withdraw function!

--- Auction/contracts/Auction.sol
+++ Auction/contracts/Auction.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.4.17;
 contract Auction {
     address owner;
     bool ended;
-    address public highestBidder;
-    uint256 public highestBid;
+    address highestBidder;
+    uint256 highestBid;
+    mapping(address => uint256) userBiddedValue;
 
     constructor (uint256 _initialBid) public {
         owner = msg.sender;
@@ -25,7 +26,9 @@ contract Auction {
     modifier auctionNotEndYet() {
         require(!ended);
         _;
-    } 
+    }
+
+    
 
     event auctionEnded(uint256 _highestBid, address _winner);
     event newHighestBid(uint256 _highestBid);
@@ -34,11 +37,25 @@ contract Auction {
         require(msg.value > highestBid, "Your bid value is too low!");
         highestBid = msg.value;
         highestBidder = msg.sender;
+        userBiddedValue[msg.sender] += msg.value;
         emit newHighestBid(msg.value);
+    }
+
+    function withdraw() public returns (uint256) {
+        if(!ended) return 0;
+        uint256 amount = userBiddedValue[msg.sender];
+        if (amount > 0) {
+            userBiddedValue[msg.sender] = 0;
+            if (!msg.sender.send(amount)) {
+                userBiddedValue[msg.sender] = amount;
+            }
+        } 
+        return amount;
     }
 
     function endBid() public onlyOwner {
         ended = true;
+        userBiddedValue[highestBidder] -= highestBid;
         emit auctionEnded(highestBid, highestBidder);
     }
 
