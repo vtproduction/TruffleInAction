6520b42043d3fe621cafdc40bc052c55e9ae6fd6
Lê Mạnh Niên <nienbkict@gmail.com>
2018-06-18 19:04:59 +0700

    test execute command TransferFrom

--- SimpleToken/truffle.js
+++ SimpleToken/truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     ganache: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
+      from: "0xDFF5A4c6ec8E01f2bEE15B9f151cC94a85cB3d07"
     }
   }
 };
